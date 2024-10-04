df13bcb73358ac77497dd2ad6d65e408cf5dfed8
Druidell <danil.kochergin.1305@mail.ru>
2024-10-04 19:57:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,16 @@
 require __DIR__ . '/product.php';
 require __DIR__ .'/client.php';
 
-$client = new Client();
-
-$client->name = "Маг";
-$client->last_order("Палка");
-$client->allMoney = 100;
-echo $client->about_client();
-
 $product = new Product();
+$product->new_information("Палка 2", "Волшебная", 250, "Шотландия");
+echo $product->about_product().'<br>';
+$product->new_cost(300);
+echo "Обновленная информация о палке:<br>" . $product->about_product().'<br>';
 
-$product->name = "Палка";
-$product->description = "Магическая";
-$product->price = 100;
-$product->country = "Шотландия";
-echo $product->about_product();
-
-$product->new_information("Палка 2");
-$product->new_cost(200);
-echo "Обновленная информация о палке:<br>" . $product->about_product();
-
+$client = new Client();
+$client->name = "Маг";
+$client->allMoney = 200;
+$client->last_order("Палка");
 $client->money(200);
-echo $client->about_client();
+echo $client->about_client().'<br>';
 ?>
