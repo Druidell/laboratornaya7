5bb0edbb3d4d93a2fa72fa33d7669050d2afae7d
Druidell <danil.kochergin.1305@mail.ru>
2024-10-04 19:58:08 +0300

    Update product.php

--- product.php
+++ product.php
@@ -2,22 +2,26 @@
 class Product {
     public $name;
     public $description;
-    public $price;
+    public $cost;
     public $country;
 
     public function about_product() {
         return "Имя: $this->name<br>
         Описание: $this->description<br>
-        Цена: $this->price<br>
+        Цена: $this->cost<br>
         Страна производиетля: $this->country<br>";
     }
 
-    public function new_information($name) {
+    public function new_information($name, $description, $cost, $country) {
         $this->name = $name;
+        $this->description = $description;
+        $this->cost = $cost;
+        $this->country = $country;
+        return $this;
     }
 
-    public function new_cost($price) {
-        $this->price = $price;  
+    public function new_cost($cost) {
+        return $this->cost = $cost;  
     }
 }
 ?>
